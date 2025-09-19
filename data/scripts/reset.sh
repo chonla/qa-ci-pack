48a41ff53deb988df0526afc74d30215da91a412
Chonlasith Jucksriporn <chonlasith@gmail.com>
2025-09-19 14:55:28 +0700

    chore: Add allure groovy to shared lib

--- data/scripts/reset.sh
+++ data/scripts/reset.sh
@@ -2,6 +2,7 @@
 
 JENKINS_HOME_PATH="./mounted/jenkins_home"
 JENKINS_INIT_GROOVY_PATH="${JENKINS_HOME_PATH}/init.groovy.d"
+JENKINS_VARS_PATH="${JENKINS_HOME_PATH}/vars"
 ASSETS_PATH="./data/assets"
 
 echo "🧹 Cleaning up jenkins_home..."
@@ -9,7 +10,11 @@ rm -rf "${JENKINS_HOME_PATH}"
 mkdir "${JENKINS_HOME_PATH}"
 chmod +w "${JENKINS_HOME_PATH}"
 mkdir "${JENKINS_INIT_GROOVY_PATH}"
+mkdir "${JENKINS_VARS_PATH}"
 
-echo "📝 Deploying initial script..."
+echo "📝 Deploying initialization script..."
 cp "${ASSETS_PATH}/bypass.groovy" "${JENKINS_INIT_GROOVY_PATH}"
 cp "${ASSETS_PATH}/generate_token.groovy" "${JENKINS_INIT_GROOVY_PATH}"
+
+echo "📝 Deploying pipeline scripts..."
+cp "${ASSETS_PATH}/allure.groovy" "${JENKINS_VARS_PATH}"
